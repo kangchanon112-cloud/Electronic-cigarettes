12c033d69fa663101db175c0c6ffa5b401a003ad
kangchanon112-cloud <kangchanon112@gmail.com>
2025-08-16 14:01:28 +0700

    Update server.js

--- server.js
+++ server.js
@@ -218,7 +218,7 @@ app.post('/save-profile', async (req, res) => {
 });
 
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'view', 'index.html'));
 });
 
